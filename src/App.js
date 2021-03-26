ea3d2190e32cba0f45612fa478f16eb7b9d641dd
deepwwwave <deepwwwave@gmail.com>
2021-03-26 17:52:36 +0100

    liste des taches a faire OK

--- src/App.js
+++ src/App.js
@@ -9,22 +9,38 @@ export default class App extends React.Component {
     this.state = {
       items : [
         {text: "learn react", key: Date.now }
-      ]
+      ],
+      input: ''
     }
   }
- 
 
+  // Methode qui enregistre la valeur du formulaire dans la variable input du state
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({ input : e.target.value})
+  }
+
+  // Méthode qui enregistre la saisie du formulaire dans la variable items du state
+  add =()=>{
+
+    let newItem = {text: this.state.input, key: Date.now()
+    }
+    this.setState(state => ({
+      items: [newItem].concat(state.items)
+    }))
+  }
+ 
   render() {
     return (
       <div id="container">
         <div className="todoListMain">
           <div className="header">
-            <form>
-              <input placeholder="enter task" className="rounded"/>
+            <form onSubmit = {(e) => {e.preventDefault() ; this.add() }}>
+              <input placeholder="enter task" className="rounded" onChange = { (e) => this.handleChange(e)}/>
               <button className="rounded">add</button>
               <ul>
                 {this.state.items.map(item => {
-                  return(<li>{item.text}</li>)
+                  return(<li key={item.key}>{item.text}</li>)
                 })}
               </ul>
             </form>
